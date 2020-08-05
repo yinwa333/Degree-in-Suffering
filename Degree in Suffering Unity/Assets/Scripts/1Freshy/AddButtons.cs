b06f62f702e73d8cd98aefb8f92c355b6af091e6
Ami Cai <ami.cai333@gmail.com>
2020-08-05 15:24:01 -0400

    Created Card Level
    Started adding 18 cards to make a matching game for freshman year, no labels or score, or turning card around, but a really good start

---
+++ Degree in Suffering Unity/Assets/Scripts/1Freshy/AddButtons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    //the Rect Transform for the Canvas/Group of cards
+    [SerializeField]
+    private Transform puzzleField;
+
+    //Create a foreloop(?) 
+    [SerializeField]
+    private GameObject btn;
+
+    private void Awake()
+    {
+        //18 Buttons
+        for (int i = 0; i < 18; i++)
+        {
+            //Instantiate creates a COPY of the thing you provide
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
+    }
+
+
+}
---
+++ Degree in Suffering Unity/Assets/Scripts/Start Menu/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadFreshman()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void LoadSophomore()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void LoadJunior()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void LoadSenior()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 4);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+}
