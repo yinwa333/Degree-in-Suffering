d534e2a686060b8544e00831b2269ea41f7386cd
Ami Cai <ami.cai333@gmail.com>
2020-08-06 09:43:01 -0400

    Started Sophomore Year
    Moving Character and walls. Following Camera works with small smoothing.

---
+++ Degree in Suffering Unity/Assets/Scripts/2Softy/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+        if(transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            //targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            //targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
---
+++ Degree in Suffering Unity/Assets/Scripts/2Softy/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D myRB;
+    private Animator myAnim;
+
+    [SerializeField]
+    private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.fixedDeltaTime;
+
+        myAnim.SetFloat("moveX", myRB.velocity.x);
+        myAnim.SetFloat("moveY", myRB.velocity.y);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAnim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAnim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
